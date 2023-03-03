2be82a3917354377ca766cc05e36ca05b9c79731
AlbanRenahy <albanrenahy.dev@gmail.com>
2023-03-03 18:13:51 +0100

    :sparkles: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.sass';
+
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
