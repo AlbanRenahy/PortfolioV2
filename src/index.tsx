adc4beaa55cd7fbbefbb8617bb9337d86bde9517
AlbanRenahy <albanrenahy.dev@gmail.com>
2023-03-04 16:46:06 +0100

    :lipstick: begin skills cards with animation

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import bemCssModules from 'bem-css-modules';
+
+import 'normalize.css';
 import './index.sass';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+bemCssModules.setSettings({ modifierDelimiter: '--' })
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,8 +17,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
